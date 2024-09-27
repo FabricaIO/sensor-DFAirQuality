366947e94222e4ced5e95ef4ff90602b1237302f
Garth Johnson <growlf@thenetyeti.com>
2024-09-26 19:09:57 -0700

    initial import

---
+++ src/DFAirQuality.cpp
@@ -0,0 +1,23 @@
+#include "DFAirQuality.h"
+
+/// @brief Creates a new air quality sensor object
+/// @param I2C_bus The I2C bus attached to the sensor
+/// @param address Address of the sensor
+DFAirQuality::DFAirQuality(TwoWire* I2C_bus, uint8_t address) : air_sensor(I2C_bus, address) {}
+
+bool DFAirQuality::begin() {
+	values.resize(2);
+	Description.parameterQuantity = 2;
+	Description.type = "Air Quality Sensor";
+	Description.name = "Air Quality Sensor";
+	Description.parameters = {"PM 2.5", "PM 10"};
+	Description.units = {"ug/m^3", "ug/m^3"};
+	Description.id = 2;
+	return air_sensor.begin();
+}
+
+bool DFAirQuality::takeMeasurement() {
+	values[0] = air_sensor.gainParticleConcentration_ugm3(PARTICLE_PM2_5_STANDARD);
+	values[1] = air_sensor.gainParticleConcentration_ugm3(PARTICLE_PM10_STANDARD);
+	return true;
+}
---
+++ src/DFAirQuality.h
@@ -0,0 +1,25 @@
+/*
+ * This file and associated .cpp file are licensed under the GPLv3 License Copyright (c) 2024 Sam Groveman
+ * 
+ * External libraries needed:
+ * DFRobot_AirQualitySensor: https://github.com/DFRobot/DFRobot_AirQualitySensor
+ * 
+ * Air Quality Sensor: https://www.dfrobot.com/product-2439.html
+ *
+ * Contributors: Sam Groveman
+ */
+
+#pragma once
+#include <Sensor.h>
+#include <Wire.h>
+#include <DFRobot_AirQualitySensor.h>
+
+class DFAirQuality : public Sensor {
+	public:
+		DFAirQuality(TwoWire* I2C_bus = &Wire, uint8_t address = 0x19);
+		bool begin();
+		bool takeMeasurement();
+
+	private:
+		DFRobot_AirQualitySensor air_sensor;
+};
